c5a8a8dcb6a90b5d64f66d921cb9059737c18d8f
igor-nasibyants <igor.nasibyants@moneyman.ru>
2019-10-30 00:27:55 +0300

    Add controller and test

---
+++ src/main/java/com/ihelper/demo/controller/DemoController.java
@@ -0,0 +1,14 @@
+package com.ihelper.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class DemoController {
+
+    @GetMapping
+    public HttpStatus status() {
+        return HttpStatus.OK;
+    }
+}
--- src/test/java/com/ihelper/demo/DemoApplicationTests.java
+++ src/test/java/com/ihelper/demo/DemoApplicationTests.java
@@ -1,10 +1,26 @@
 package com.ihelper.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoApplicationTests {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void demoStatus() throws Exception {
+		mvc.perform(get("/"))
+				.andExpect(status().isOk());
+	}
 
 	@Test
 	void contextLoads() {
