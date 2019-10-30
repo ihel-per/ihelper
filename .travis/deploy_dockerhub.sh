19dcfd5f2e3a7036c0f1e558471e7ad262819761
igor-nasibyants <igor.nasibyants@moneyman.ru>
2019-10-31 00:52:45 +0300

    simplify docker image push script

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-    TAG="latest"
-else
-    TAG="$TRAVIS_BRANCH"
-fi
-docker build -f Dockerfile -t $DOCKER_USER:$TAG .
-docker push ihelper/ihelper:$TAG
+docker build -f Dockerfile -t ihelper:latest .
+docker push ihelper/ihelper:latest
