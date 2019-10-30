69e6c9f6603fce19614fec84c7dd97f09f5e85ea
igor-nasibyants <igor.nasibyants@moneyman.ru>
2019-10-31 01:10:04 +0300

    Remove email from env variables

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker build -f Dockerfile -t ihelper:latest .
 docker push ihelper:latest
