e1c1fdc654a07155bd161746534d7428c631d113
igor-nasibyants <igor.nasibyants@moneyman.ru>
2019-11-01 11:56:12 +0300

    Change login command

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install @heroku-cli/plugin-container-registry
-docker login -u ihel.per@mail.ru  --password=$HEROKU_API_KEY registry.heroku.com
+#docker login -u ihel.per@mail.ru  --password=$HEROKU_API_KEY registry.heroku.com
+docker login -u ihel.per@mail.ru  --password=(SyF9ay:MzMT$(^' registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
